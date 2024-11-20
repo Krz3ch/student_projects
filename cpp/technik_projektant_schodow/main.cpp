fcc588524815248217c78d703f642027a1a93a61
Krzysztof Gadzina <89007379+Krz3ch@users.noreply.github.com>
2024-11-20 08:45:59 +0000

    Update main.cpp

--- cpp/technik_projektant_schodow/main.cpp
+++ cpp/technik_projektant_schodow/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include "Stairs.cpp"
 
 using namespace std;
 
 int main(){
-	cout<<"witaj swiecie"<<endl;
+	
+	Stairs stairs = makeStairs(25000, 6000);
+
+	cout<<stairs.stepCount<<", "<<stairs.stepHeight<<", "<<stairs.stepLength;
 	return 0;
 }
