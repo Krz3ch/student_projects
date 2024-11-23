ae07858fa0414eb1db63f1b4aab5d37c906d4412
Krzysztof Gadzina <89007379+Krz3ch@users.noreply.github.com>
2024-11-23 18:33:25 +0100

    Update Stairs.cpp

--- cpp/technik_projektant_schodow/Stairs.cpp
+++ cpp/technik_projektant_schodow/Stairs.cpp
@@ -5,38 +5,39 @@ using namespace std;
 
 class Stairs
 {
-    public:
-        int stepHeight;
-        int stepLength;
-        int stepCount;
-        bool canBeMade = false;
-    Stairs (int height, int length, int count, bool isOk) : stepHeight(height), stepLength(length), stepCount(count), canBeMade(isOk){}
+public:
+    int stepHeight;
+    int stepLength;
+    int stepCount;
+    bool canBeMade = false;
+    Stairs(int height, int length, int count, bool isOk) : stepHeight(height), stepLength(length), stepCount(count), canBeMade(isOk) {}
 };
 
-Stairs makeStairs(int kHeight, int kLength){
-    
+Stairs makeStairs(int kHeight, int kLength) {
+
     int stairsCount = 0, stairsHeight, stairsLength, test;
 
-    for(int i = 150; i < 170; i++){
-        if(kHeight % i == 0){
+    for (int i = 150; i < 170; i++) {
+        if (kHeight % i == 0) {
             test = kHeight / i;
             stairsCount = test;
             break;
         }
     }
 
-    if(stairsCount == 0){
-        if(round(kHeight/150) == 0){
+    if (stairsCount == 0) {
+        if (round(kHeight / 150) == 0) {
             stairsCount = 1;
-        }else{
-            stairsCount = round(kHeight/150);
+        }
+        else {
+            stairsCount = round(kHeight / 150);
         }
     }
 
-    stairsHeight = kHeight/stairsCount;
-    stairsLength = kLength/stairsCount;
-    
-    if(stairsLength < 290){
+    stairsHeight = kHeight / stairsCount;
+    stairsLength = kLength / stairsCount;
+
+    if (stairsLength < 290) {
         return Stairs(0, 0, 0, false);
     }
 
